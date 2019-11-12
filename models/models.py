1199c8ccf946da25c12a1e552d5f9315a002ad00
karens9414 <karen.suarez9414@gmail.com>
2019-11-12 22:47:56 +0000

    Modificacion de Models.py

--- models/models.py
+++ models/models.py
@@ -2,13 +2,13 @@
 
 from odoo import models, fields, api
 
-# class open_academy(models.Model):
-#     _name = 'open_academy.open_academy'
-
-#     name = fields.Char()
+   class Course(models.Model)
+     _name = 'open_academy.course'
+     _description= "OpenAcademy Courses"
+     name = fields.Char(string="Title",reqquired=True)
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+     description = fields.Text()
 #
 #     @api.depends('value')
 #     def _value_pc(self):
