9a4d252ed3957a732fe50f72e7dfa52a716d6c2c
karens1494 <57648917+karens1494@users.noreply.github.com>
2019-11-14 13:45:05 -0600

    Update

--- models/models.py
+++ models/models.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
    class Course(models.Model)
-     _name = 'openacademy.course'
+     _name = 'open_academy.course'
      _description= "OpenAcademy Courses"
      responsible_id = fields.Many2one('res.users',
         ondelete='set null', string="Responsible", index=True)
@@ -16,12 +16,12 @@ from odoo import models, fields, api
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
 class Session(models.Model):
-    _name = 'openacademy.session'
+    _name = 'open_academy.session'
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
     instructor_id = fields.Many2one('res.partner', string="Instructor")
-    course_id = fields.Many2one('openacademy.course',
+    course_id = fields.Many2one('open_academy.course',
         ondelete='cascade', string="Course", required=True)
        attendee_ids = fields.Many2many('res.partner', string="Attendees")
